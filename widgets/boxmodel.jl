2772d773a0739817ce852908ebfa515a9a779e16
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2020-05-24 22:51:42 -0300

    Create boxmodel.jl

---
+++ widgets/boxmodel.jl
@@ -0,0 +1,45 @@
+```julia
+rectangle
+(
+  box_sizing = "content_box",
+  box_sizing = "border_box",
+  box_sizing = "initial",
+  box_sizing = "inherit",
+
+
+  size = (width = 40 "vmax", height = 60 "vmin"),
+  size = (40 "vmax", 60 "vmin"),
+
+  width = 40 "vmax",
+  height = 60 "vmin",
+
+
+  size_container = (width = 40 "vmax", height = 60 "vmin"),
+  size_container = (40 "vmax", 60 "vmin"),
+
+  width_container = 40 "vmax",
+  height_container = 60 "vmin",
+
+
+  size_content = (width = 40 "vmax", height = 60 "vmin"),
+  size_content = (40 "vmax", 60 "vmin"),
+
+  width_content = 40 "vmax",
+  height_content = 60 "vmin",
+
+
+  padding = (top = 10 "px", right = 50 "px", bottom = 30 "px", left = 15 "px"),
+  padding = (10 "px", 50 "px", 30 "px", 15 "px"),
+  padding = (top = 10 "px", horizontal = 50 "px", bottom = 30 "px"),
+  padding = (10 "px", 50 "px", 30 "px"),
+  padding = (vertical = 10 "px", horizontal = 50 "px"),
+  padding = (10 "px", 50 "px"),
+  padding = (all = 10 "px"),
+  padding = (10 "px"),
+
+  
+)
+do
+  # content
+end
+```
